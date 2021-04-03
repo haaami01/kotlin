70aab99ef0d8f01dca01a78f212740e89f6f2451
Tianyu Geng <tgeng@google.com>
2021-04-02 20:49:50 -0700

    FIR: add equality call checker
    Added checker for FirEqualityOperatorCall. It's surfaced as one of the
    following diagnostics depending on the PSI structure and types under
    comparison:
    
    * INCOMPATIBLE_TYPES(_WARNING)
    * EQUALITY_NOT_APPLICABLE(_WARNING)
    * INCOMPATIBLE_ENUM_COMPARISON_ERROR
    
    Comparing with FE1.0, the current implementation is more conservative
    and only highlights error if the types are known to follow certain
    contracts with `equals` method. Otherwise, the checker reports warnings
    instead.
    
    However, the current checker is more strict in the following situations:
    1. it now rejects incompatible enum types like `Enum<E1>` and
      `Enum<E2>`, which was previously accepted
    2. it now rejects incompatible class types like `Class<String>` and
      `Class<Int>`, which was previously accepted
    3. the check now takes smart cast into consideration, so
      `if (x is String) x == 3` is now rejected

--- compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/checkers/CommonExpressionCheckers.kt
+++ compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/checkers/CommonExpressionCheckers.kt
@@ -103,4 +103,9 @@ object CommonExpressionCheckers : ExpressionCheckers() {
         get() = setOf(
             FirStandaloneQualifierChecker,
         )
+
+    override val equalityOperatorCallCheckers: Set<FirEqualityOperatorCallChecker>
+        get() = setOf(
+            FirEqualityCompatibilityChecker,
+        )
 }
